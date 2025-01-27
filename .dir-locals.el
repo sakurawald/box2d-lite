910d77f3e206a37c62da1ae70cf3613e7aceb08f
sakurawald <sakurawald@gmail.com>
2025-01-27 10:46:18 +0800

    add: lldb debugger configuration + debug build type

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,10 @@
 ((nil . ((projectile-project-compilation-cmd . "bash build.sh")
-	 (projectile-project-run-cmd . "bash build.sh && ./build/samples/samples"))))
+	 (projectile-project-run-cmd . "bash build.sh && ./build/samples/samples")
+	 (eval . (progn
+		   (dap-register-debug-template
+		    "LLDB::Run (samples)"
+		    (list :type "lldb-vscode"
+			  :cwd "/home/sakurawald/Workspace/github/box2d-lite/build/samples"
+			  :request "launch"
+			  :program "samples"
+			  :name "LLDB::Run")))))))
